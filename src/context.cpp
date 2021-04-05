37c555ef308ae419782c15cb1fd8d600bfbb2cd1
Moon Seok Kyu <lico15@kangwon.ac.kr>
2021-04-05 20:28:37 +0900

    5주차 이미지 텍스쳐 입히기

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,5 @@
 #include "context.h"
+#include "image.h"
 
 ContextUPtr Context::Create() {
     auto context = ContextUPtr(new Context());
@@ -9,12 +10,13 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
         float vertices[] = {
-            	
-             0.5f, 0.5f, 0.0f, 0.2f, 0.2f, 0.2f, // top right, color
-             0.5f, -0.5f, 0.0f, 0.5f, 0.0f, 0.0f, // bottom right, color
-            -0.5f, -0.5f, 0.0f, 0.8f, 0.8f, 0.8f, // bottom left, color
-            -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.5f, // top left, color
+
+             0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
         };
+                
 
         uint32_t indices[] = { // note that we start from 0!
             0, 1, 3, // first triangle
@@ -23,13 +25,13 @@ bool Context::Init() {
 	
     m_vertexLayout = VertexLayout::Create();
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, 
-        GL_STATIC_DRAW, vertices, sizeof(float) * 24);
+        GL_STATIC_DRAW, vertices, sizeof(float) * 32);
     
-    /// m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-    
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
-    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
-    
+    /// m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);    
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3);
+    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
+
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
  
     /// simple.vs, simple.fs
@@ -38,8 +40,12 @@ bool Context::Init() {
     ///ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
     
     /// per_vertex_color.vs, per_vertex_color.vs
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    ///ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ///ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    
+    ///texture.vs, texture.fs
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
 
     if (!vertShader || !fragShader)
         return false;
@@ -52,6 +58,31 @@ bool Context::Init() {
     SPDLOG_INFO("program id: {}", m_program->Get());
 
         glClearColor(0.5f, 0.5f, 0.2f, 0.0f);
+
+    ///텍스쳐 이미지 로딩
+    auto image = Image::Load("./image/container.jpg");
+    if (!image) 
+        return false;
+    SPDLOG_INFO("image: {}x{}, {} channels",
+        image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+	
+    ///체크무늬 생성
+    ///auto image = Image::Create(512, 512);
+    ///image->SetCheckImage(16, 16);
+
+    m_texture = Texture::CreateFromImage(image.get());
+
+    auto image2 = Image::Load("./image/awesomeface.png");
+    m_texture2 = Texture::CreateFromImage(image2.get());
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture->Get());
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, m_texture2->Get());
+
+    m_program->Use();
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex"), 0);
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex2"), 1);
 
     return true;
 }
